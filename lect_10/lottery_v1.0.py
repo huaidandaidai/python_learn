171d5543d409f3e9864b469a680c6cf8671c13ae
huaidandaidai <1004928908@qq.com>
2019-01-24 10:14:39 +0800

    1、修改获取期数和每期详情链接的方法

--- lect_10/lottery_v1.0.py
+++ lect_10/lottery_v1.0.py
@@ -9,20 +9,26 @@ from bs4 import BeautifulSoup
 
 
 def get_batch_number():
-    url = "http://kaijiang.500.com/shtml/ssq/"
-    batch_number = []
-    for i in range(3001, 19009):
-        if len(str(i)) == 4:
-            batch_number.append("0" + str(i))
-        else:
-            batch_number.append(str(i))
-    return batch_number
+    """
+        获取期数
+    """
+    url = "http://kaijiang.500.com/shtml/ssq/19009.shtml"
+    r = requests.get(url, timeout=30)
+    soup = BeautifulSoup(r.text, 'lxml')
+    number_div = soup.find_all('div',{'class': 'iSelectList'})[0]
+    number_links = number_div.find_all("a")
+    batch_number_list = []
+    for link in number_links:
+        batch_number = link.text
+        batch_number_link = link['href']
+        batch_number_list.append((batch_number, batch_number_link))
+    return batch_number_list
 
 
-def get_double_chromosphere(batch_number):
+def get_double_chromosphere(batch_number_list):
 
-    for num in batch_number:
-        r = requests.get('http://kaijiang.500.com/shtml/ssq/'+num+'.shtml', timeout=30)
+    for batch_number in batch_number_list:
+        r = requests.get(batch_number[1], timeout=30)
         soup = BeautifulSoup(r.text, 'lxml')
         number_div = soup.find_all('div', {'class': 'ball_box01'})[0]
         red_ball_li = number_div.find_all('li', {'class': 'ball_red'})
@@ -31,16 +37,15 @@ def get_double_chromosphere(batch_number):
         for red_ball in red_ball_li:
             red_ball_list.append(red_ball.text)
 
-        print("期数：",num,"红球：",red_ball_list, "蓝球：", blue_ball)
+        print("期数：",batch_number[0],"红球：",red_ball_list, "蓝球：", blue_ball)
 
 
 def main():
     """
         主函数
     """
-    url = "http://kaijiang.500.com/shtml/ssq/"
-    batch_number = get_batch_number()
-    get_double_chromosphere(batch_number)
+    batch_number_list = get_batch_number()
+    get_double_chromosphere(batch_number_list)
 
 
 if __name__ == '__main__':
