dacdb27cbd150daaa56bd48016ef9d8d0b3aedef
lishanhui <1004928908@qq.com>
2019-09-14 23:27:14 +0800

    lect_17 方法结构

--- lect_17/database_2_doc.py
+++ lect_17/database_2_doc.py
@@ -4,6 +4,29 @@
     日期：2019-09-07
     功能：数据库表结构导出为文档
 """
+
+"""
+连接数据库
+"""
+def connectDatabase():
+    pass
+
+
+"""
+解析数据库表结构
+"""
+def analysisDatabase():
+    pass
+
+
+"""
+将解析内容写入文件
+"""
+def writeDoc():
+    pass
+
+
+
 def main():
     pass
 
